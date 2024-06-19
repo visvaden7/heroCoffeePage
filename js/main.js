3a8cf7a65ad9c0d1fb5243e39806fc1b124650b5
visvaden7 <visvaden7@naver.com>
2024-06-19 20:43:57 +0900

    feat: top버튼 기능 완료

--- js/main.js
+++ js/main.js
@@ -32,3 +32,40 @@ new Swiper(".awards .swiper", {
 
 const thisYear = document.querySelector(".this-year");
 thisYear.innerHTML = new Date().getFullYear();
+
+const badgeEl = document.querySelector("header .badges");
+const toTopEl = document.querySelector("#to-top");
+
+toTopEl.addEventListener("click", () => {
+  console.log(0);
+  gsap.to(window, 0.6, {
+    scrollTo: 0,
+  });
+});
+
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    // gsap.to(badgeEl, 0.6, {
+    //   opacity: 0,
+    //   display: "none",
+    // });
+
+    //상단으로 이동 버튼 보이기
+    gsap.to(toTopEl, 0.6, {
+      opacity: 1,
+      x: 0,
+    });
+  } else {
+    // gsap.to(badgeEl, 0.6, {
+    //   opacity: 0,
+    //   display: "none",
+    // });
+
+    //상단으로 이동 버튼 숨기기
+    gsap.to(toTopEl, 0.6, {
+      opacity: 0,
+      x: 100,
+    });
+  }
+});
