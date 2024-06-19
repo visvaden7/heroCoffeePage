9469eda912da34984e7483d9bf51d845bcff3dd6
visvaden7 <visvaden7@naver.com>
2024-06-19 14:58:04 +0900

    feat: icon 추가, sub-menu 추가, sub-menu css추가, (click, focus, blur) event 추가

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//검색창 요소(.search) 구하기
+const searchEl = document.querySelector(".search");
+const searchInputEl = document.querySelector("input");
+
+//검색창 요소 클릭하면 실행
+searchEl.addEventListener("click", () => {
+  console.log(searchEl);
+  searchInputEl.focus();
+});
+
+//검색창 focus되면 늘어나면서 attribute placeholder 에 통합검색에 들어감
+searchInputEl.addEventListener("focus", () => {
+  searchEl.classList.add("focused");
+  searchInputEl.setAttribute("placeholder", "통합검색");
+});
+
+//검색창 요소 내부 실제 input 요소에서 focus 해제
+searchInputEl.addEventListener("blur", () => {
+  searchEl.classList.remove("focused");
+  searchInputEl.setAttribute("placeholder", "");
+});
